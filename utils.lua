7b39f2998a6cb2012b0e45479ebd36c1e78a17d2
Léa Wawro <leawawro001@gmail.com>
2022-03-12 17:22:05 +0100

    add utils file

---
+++ utils.lua
@@ -0,0 +1,15 @@
+
+function dist(pX1,pY1,pX2,pY2)
+  return ((pX2-pX1)^2+(pY2-pY2)^2)^0.5
+end
+
+function checkCollision(pX1,pY1,pW1,pH1,pX2,pY2,pW2,pH2)
+  
+  if pX1+pW1>=pX2 and pX1<=pX2+pW2 and 
+    pY1+pH1>=pY2 and pY1<=pY2=pH2 then
+      
+    return true
+  end
+  
+  return false
+end
